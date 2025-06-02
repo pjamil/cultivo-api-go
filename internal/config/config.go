63d08add5c64166914912d4dc7ec8735270a27cb
Paulo Jamil <paulojamil@gmail.com>
2025-06-01 22:11:38 -0300

    Refatora docker compose

--- internal/config/config.go
+++ internal/config/config.go
@@ -27,7 +27,7 @@ func LoadConfig() *Config {
 		DBHost:     getEnv("DB_HOST", "localhost"),
 		DBPort:     getEnv("DB_PORT", "5432"),
 		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "753951465827PJamil#*"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
 		DBName:     getEnv("DB_NAME", "cultivo-api-go"),
 		ServerPort: getEnv("SERVER_PORT", "8080"),
 	}
