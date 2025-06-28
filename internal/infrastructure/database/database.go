258773937f796e989ea8d8f9531a7b70fb040634
Paulo Jamil <paulojamil@gmail.com>
2025-06-28 15:14:56 -0300

    refactor: remove gorm AutoMigrate

--- internal/infrastructure/database/database.go
+++ internal/infrastructure/database/database.go
@@ -8,8 +8,7 @@ import (
 	"gorm.io/gorm"
 
 	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/config"
-	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/domain/models"
-)
+	)
 
 type Database struct {
 	DB *gorm.DB
@@ -26,33 +25,6 @@ func NewDatabase(config *config.Config) (*Database, error) {
 
 	log.Println("Database connection established")
 
-	// Auto migrate models
-	if err := db.AutoMigrate(
-		&models.Ambiente{},
-		&models.Anotacao{},
-		&models.ClimaRegistro{},
-		&models.ColecaoMidia{},
-		&models.DiarioCultivo{},
-		&models.EstagioCrescimento{},
-		&models.Foto{},
-		&models.Genetica{},
-		&models.MeioCultivo{},
-		&models.Microclima{},
-		&models.Midia{},
-		&models.Planta{},
-		&models.RegistroCrescimento{},
-		&models.RegistroDiario{},
-		&models.RegistroPlanta{},
-		&models.Substrato{},
-		&models.Tarefa{},
-		&models.TarefaTemplate{},
-		&models.TarefaPlanta{},
-		&models.Usuario{},
-		&models.Vaso{},
-	); err != nil {
-		return nil, fmt.Errorf("failed to migrate database: %w", err)
-	}
-
 	// Check database connection
 	sqlDB, err := db.DB()
 	if err != nil {
@@ -63,18 +35,4 @@ func NewDatabase(config *config.Config) (*Database, error) {
 	}
 
 	return &Database{DB: db}, nil
-}
-
-// Adicione no init() ou em migrations:
-func SetupRelationships(db *gorm.DB) {
-	// Planta pertence a Ambiente e Vaso
-	db.SetupJoinTable(&models.Planta{}, "Ambiente", &models.Ambiente{})
-	db.SetupJoinTable(&models.Planta{}, "Vaso", &models.Vaso{})
-
-	// Tarefas podem ter múltiplas fotos
-	db.SetupJoinTable(&models.Tarefa{}, "Fotos", &models.Foto{})
-
-	// Índices para melhor performance
-	db.Migrator().CreateIndex(&models.Tarefa{}, "status")
-	db.Migrator().CreateIndex(&models.Planta{}, "status")
 }
