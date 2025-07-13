5b774656dcb5d3688e61415b2d86616d649b7be6
Paulo Jamil <paulojamil@gmail.com>
2025-07-13 17:32:35 -0300

    fix: Remove campo Orientacao de testes de ambiente
    Remove o campo 'Orientacao' da inicialização de 'CreateAmbienteDTO'
    nos testes de ambiente, pois este campo não faz mais parte da
    estrutura, corrigindo falhas nos testes.

--- internal/domain/service/ambiente_service_test.go
+++ internal/domain/service/ambiente_service_test.go
@@ -1,54 +1,25 @@
 package service_test
 
 import (
-	"encoding/json"
 	"errors"
 	"testing"
 
 	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/domain/dto"
 	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/domain/models"
 	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/domain/service"
+	"gitea.paulojamil.dev.br/paulojamil.dev.br/cultivo-api-go/internal/domain/service/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"gorm.io/gorm"
 )
 
-type MockAmbienteRepositorio struct {
-	mock.Mock
-}
-
-func (m *MockAmbienteRepositorio) Criar(ambiente *models.Ambiente) error {
-	args := m.Called(ambiente)
-	return args.Error(0)
-}
-
-func (m *MockAmbienteRepositorio) ListarTodos(page, limit int) ([]models.Ambiente, int64, error) {
-	args := m.Called(page, limit)
-	return args.Get(0).([]models.Ambiente), args.Get(1).(int64), args.Error(2)
-}
-
-func (m *MockAmbienteRepositorio) BuscarPorID(id uint) (*models.Ambiente, error) {
-	args := m.Called(id)
-	return args.Get(0).(*models.Ambiente), args.Error(1)
-}
-
-func (m *MockAmbienteRepositorio) Atualizar(ambiente *models.Ambiente) error {
-	args := m.Called(ambiente)
-	return args.Error(0)
-}
-
-func (m *MockAmbienteRepositorio) Deletar(id uint) error {
-	args := m.Called(id)
-	return args.Error(0)
-}
-
 func TestAmbienteService_Criar(t *testing.T) {
-	mockRepo := new(MockAmbienteRepositorio)
+	mockRepo := new(test.MockAmbienteRepositorio)
 	service := service.NewAmbienteService(mockRepo)
 
 	t.Run("Success", func(t *testing.T) {
-												createDTO := &dto.CreateAmbienteDTO{Nome: "Ambiente Teste", Descricao: "Descrição do ambiente"}
-		expectedResponse := &dto.AmbienteResponseDTO{ID: 1, Nome: "Ambiente Teste", Descricao: "Descrição do ambiente"}
+		createDTO := &dto.CreateAmbienteDTO{Nome: "Ambiente Teste", Tipo: "interno", Comprimento: 10, Altura: 5, Largura: 8, TempoExposicao: 12}
+		expectedResponse := &dto.AmbienteResponseDTO{ID: 1, Nome: "Ambiente Teste", Tipo: "interno", Comprimento: 10, Altura: 5, Largura: 8, TempoExposicao: 12, Orientacao: "Norte"}
 
 		mockRepo.On("Criar", mock.AnythingOfType("*models.Ambiente")).Run(func(args mock.Arguments) {
 			ambiente := args.Get(0).(*models.Ambiente)
@@ -65,7 +36,7 @@ func TestAmbienteService_Criar(t *testing.T) {
 	})
 
 	t.Run("Error - Repository Error", func(t *testing.T) {
-		createDTO := &dto.CreateAmbienteDTO{Nome: "Ambiente Teste", Descricao: "Descrição do ambiente"}
+		createDTO := &dto.CreateAmbienteDTO{Nome: "Ambiente Teste", Tipo: "interno", Comprimento: 10, Altura: 5, Largura: 8, TempoExposicao: 12}
 
 		mockRepo.On("Criar", mock.AnythingOfType("*models.Ambiente")).Return(errors.New("erro no repositório")).Once()
 
@@ -73,42 +44,42 @@ func TestAmbienteService_Criar(t *testing.T) {
 
 		assert.Error(t, err)
 		assert.Nil(t, response)
-		assert.EqualError(t, err, "erro no repositório")
+		assert.EqualError(t, err, "falha ao criar ambiente com nome Ambiente Teste: erro no repositório")
 		mockRepo.AssertExpectations(t)
 	})
 }
 
 func TestAmbienteService_ListarTodos(t *testing.T) {
-	mockRepo := new(MockAmbienteRepositorio)
+	mockRepo := new(test.MockAmbienteRepositorio)
 	service := service.NewAmbienteService(mockRepo)
 
 	t.Run("Success - Ambientes Encontrados", func(t *testing.T) {
-		expectedAmbientes := []models.Ambiente{
-			{Nome: "Ambiente 1"},
-			{Nome: "Ambiente 2"},
+		// Arrange
+		mockAmbientes := []models.Ambiente{
+			{Model: gorm.Model{ID: 1}, Nome: "Ambiente 1", Tipo: "interno"},
+			{Model: gorm.Model{ID: 2}, Nome: "Ambiente 2", Tipo: "externo"},
 		}
-		for i := range expectedAmbientes {
-			expectedAmbientes[i].ID = uint(i + 1)
-		}
-		expectedTotal := int64(len(expectedAmbientes))
+		expectedTotal := int64(len(mockAmbientes))
 		page := 1
 		limit := 10
 
-		mockRepo.On("ListarTodos", page, limit).Return(expectedAmbientes, expectedTotal, nil).Once()
+		expectedResponseData := []dto.AmbienteResponseDTO{
+			{ID: 1, Nome: "Ambiente 1", Tipo: "interno"},
+			{ID: 2, Nome: "Ambiente 2", Tipo: "externo"},
+		}
 
+		mockRepo.On("ListarTodos", page, limit).Return(mockAmbientes, expectedTotal, nil).Once()
+
+		// Act
 		response, err := service.ListarTodos(page, limit)
 
+		// Assert
 		assert.NoError(t, err)
 		assert.NotNil(t, response)
 		assert.Equal(t, expectedTotal, response.Total)
 		assert.Equal(t, page, response.Page)
 		assert.Equal(t, limit, response.Limit)
-
-		actualAmbientesBytes, _ := json.Marshal(response.Data)
-		var actualAmbientes []models.Ambiente
-		json.Unmarshal(actualAmbientesBytes, &actualAmbientes)
-
-		assert.Equal(t, expectedAmbientes, actualAmbientes)
+		assert.Equal(t, expectedResponseData, response.Data)
 		mockRepo.AssertExpectations(t)
 	})
 
@@ -141,14 +112,14 @@ func TestAmbienteService_ListarTodos(t *testing.T) {
 }
 
 func TestAmbienteService_BuscarPorID(t *testing.T) {
-	mockRepo := new(MockAmbienteRepositorio)
+	mockRepo := new(test.MockAmbienteRepositorio)
 	service := service.NewAmbienteService(mockRepo)
 
-		t.Run("Success", func(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
 		ambienteID := uint(1)
-		expectedAmbiente := &models.Ambiente{Nome: "Ambiente Teste"}
+		expectedAmbiente := &models.Ambiente{Nome: "Ambiente Teste", Tipo: "interno"}
 		expectedAmbiente.ID = ambienteID
-		expectedResponse := &dto.AmbienteResponseDTO{ID: ambienteID, Nome: "Ambiente Teste"}
+		expectedResponse := &dto.AmbienteResponseDTO{ID: ambienteID, Nome: "Ambiente Teste", Tipo: "interno"}
 
 		mockRepo.On("BuscarPorID", ambienteID).Return(expectedAmbiente, nil).Once()
 
@@ -158,6 +129,7 @@ func TestAmbienteService_BuscarPorID(t *testing.T) {
 		assert.NotNil(t, response)
 		assert.Equal(t, expectedResponse.ID, response.ID)
 		assert.Equal(t, expectedResponse.Nome, response.Nome)
+		assert.Equal(t, expectedResponse.Tipo, response.Tipo)
 		mockRepo.AssertExpectations(t)
 	})
 
@@ -190,15 +162,15 @@ func TestAmbienteService_BuscarPorID(t *testing.T) {
 }
 
 func TestAmbienteService_Atualizar(t *testing.T) {
-	mockRepo := new(MockAmbienteRepositorio)
+	mockRepo := new(test.MockAmbienteRepositorio)
 	service := service.NewAmbienteService(mockRepo)
 
 	t.Run("Success", func(t *testing.T) {
 		ambienteID := uint(1)
 		updateDTO := &dto.UpdateAmbienteDTO{Nome: "Ambiente Atualizado"}
-		existingAmbiente := &models.Ambiente{Nome: "Ambiente Antigo"}
+		existingAmbiente := &models.Ambiente{Nome: "Ambiente Antigo", Tipo: "interno"}
 		existingAmbiente.ID = ambienteID
-		expectedResponse := &dto.AmbienteResponseDTO{ID: ambienteID, Nome: "Ambiente Atualizado"}
+		expectedResponse := &dto.AmbienteResponseDTO{ID: ambienteID, Nome: "Ambiente Atualizado", Tipo: "interno"}
 
 		mockRepo.On("BuscarPorID", ambienteID).Return(existingAmbiente, nil).Once()
 		mockRepo.On("Atualizar", mock.AnythingOfType("*models.Ambiente")).Run(func(args mock.Arguments) {
@@ -211,6 +183,7 @@ func TestAmbienteService_Atualizar(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotNil(t, response)
 		assert.Equal(t, expectedResponse.Nome, response.Nome)
+		assert.Equal(t, expectedResponse.Tipo, response.Tipo)
 		mockRepo.AssertExpectations(t)
 	})
 
@@ -228,10 +201,10 @@ func TestAmbienteService_Atualizar(t *testing.T) {
 		mockRepo.AssertExpectations(t)
 	})
 
-		t.Run("Repository Error on Update", func(t *testing.T) {
-				ambienteID := uint(1)
+	t.Run("Repository Error on Update", func(t *testing.T) {
+		ambienteID := uint(1)
 		updateDTO := &dto.UpdateAmbienteDTO{Nome: "Ambiente Atualizado"}
-		existingAmbiente := &models.Ambiente{Nome: "Ambiente Antigo"}
+		existingAmbiente := &models.Ambiente{Nome: "Ambiente Antigo", Tipo: "interno"}
 		existingAmbiente.ID = ambienteID
 		expectedError := errors.New("erro no repositório")
 
@@ -248,7 +221,7 @@ func TestAmbienteService_Atualizar(t *testing.T) {
 }
 
 func TestAmbienteService_Deletar(t *testing.T) {
-	mockRepo := new(MockAmbienteRepositorio)
+	mockRepo := new(test.MockAmbienteRepositorio)
 	service := service.NewAmbienteService(mockRepo)
 
 	t.Run("Success", func(t *testing.T) {
